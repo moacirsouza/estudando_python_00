7284c15af5e359b077d28a74173814021e25db0b
Moka <25541880+moacirsouza@users.noreply.github.com>
2025-12-17 07:55:27 -0300

    Cria o arquivo lab-05.py

---
+++ 00-moka/lab-05.py
@@ -0,0 +1,11 @@
+john = 3
+mary = 5
+adam = 6
+print(john, mary, adam, sep=", ")
+total_apples = john + mary + adam
+print(total_apples)
+print("[+]  Total number of apples:", total_apples)
+print("[-]  Number of apples that Adam has more than Mary:", (adam - mary))
+print("[/]  John has half the number of apples that Adam do, that is:", (adam/2))
+print("[//] The last sentence might be expressed in a different way, using an integer:", (adam//2))
+print("[*]  If we multiply the number of apples John and Mary have together, we will obtain:", (john*mary))
