f8936f41f67e2457a3bad785338698139a681fb4
Moka <25541880+moacirsouza@users.noreply.github.com>
2025-12-22 08:29:50 -0300

    Cria o arquivo lab-08.py

---
+++ 00-moka/lab-08.py
@@ -0,0 +1,9 @@
+# Computes the number of seconds in 3 hours
+
+hours = 3
+seconds_in_one_hour = 3600
+
+print("Number of hours:", hours)
+print("Seconds in those hours:", hours * seconds_in_one_hour)
+print("Goodbye")
+
