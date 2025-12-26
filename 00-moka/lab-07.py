4b83728d2c26d9fccb49a9dbf13fd11863c97d9a
Moka <25541880+moacirsouza@users.noreply.github.com>
2025-12-26 11:24:16 -0300

    Reescreve comentários para melhor elucidação
    1. Coloca os valores de referência de 'x' e 'y' em uma tabela, para facilitar
    o entendimento e evitar reescrita desnecessária de termos
    2. Ajusta a posição e alinhamento dos comentários para aproveitar melhor o
    espaço disponível, respeitando o limite das 80 colunas de caracteres
    3. Alguns comentários foram reescritos para esclarecer os exemplos e elucidar
    como utilizá-los corretamente

--- 00-moka/lab-07.py
+++ 00-moka/lab-07.py
@@ -1,19 +1,28 @@
-### Valores de referência para x
-### x = 0, y = -1.0
-### x = 1, y = 3.0
-### x = -1, y = -9.0
-x = 0 # hardcode your test data here
+### Valores de referência para x e y
+###
+### +----+------+
+### | x  |  y   |
+### +----+------+
+### | 0  | -1.0 |
+### +----+------+
+### | 1  | 3.0  |
+### +----+------+
+### | -1 | -9.0 |
+### +----+------+
 
-### Não é obrigatório forçar x a ser float,
-### mas fica claro que fazendo isto, todos
-### os resultados retornam com este tipo
+# hardcode your test data here
+x = 0
+
+### Não é obrigatório forçar x a ser float, mas fica claro que, fazendo isto,
+### todos os resultados retornam com este tipo
 x = float(x)
 
-y=(3*(x**3)) - (2*(x**2)) + 3*x - 1
-### O uso dos parênteses, neste caso, não altera
-### o resultado da operação, embora aumente a
-### legibilidade do código
+### Neste caso, o uso dos parênteses não altera o resultado da operação, embora
+### aumente a legibilidade do código. O teste pode ser realizado facilmente,
+### alternando a linha em que 'y' recebe a respectiva atribuição por qualquer
+### uma das linhas comentadas abaixo
 #y=(3*x**3) - (2*x**2) + 3*x - 1
 #y=3*x**3 - 2*x**2 + 3*x - 1
+y=(3*(x**3)) - (2*(x**2)) + 3*x - 1
 
 print("y =", y)
