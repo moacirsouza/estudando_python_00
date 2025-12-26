8a9b71cb3672eb4e811e3a11e5c51d06e458e5ef
Moka <25541880+moacirsouza@users.noreply.github.com>
2025-12-26 08:35:47 -0300

    Cria o arquivo lab-09.py

---
+++ 00-moka/lab-09.py
@@ -0,0 +1,18 @@
+# input a float value for variable a here
+a = float( input("Enter the first number: ") )
+# input a float value for variable b here
+b = float( input("Enter the second number: ") )
+# output the result of addition here
+addition = a + b
+print("The result of adding", a, "and", b, "is:", addition)
+# output the result of subtraction here
+subtraction = a - b
+print("The result of subtracting", b, "from", a, "is:", subtraction)
+# output the result of multiplication here
+multiplication = a * b
+print("The result of multiplying", a, "by", b, "is:", multiplication)
+# output the result of division here
+division = a / b
+print("The result of dividing", a, "by", b, "is:", division)
+
+print("\nThat's all, folks!")
