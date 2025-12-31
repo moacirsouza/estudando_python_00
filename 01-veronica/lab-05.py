8f28acf1bec79bdec95f948daee7a9442e71bc77
Veronica <veronicapaviani@gmail.com>
2025-12-31 08:14:43 -0300

    exercicio lab-05 corrigido

--- 01-veronica/lab-05.py
+++ 01-veronica/lab-05.py
@@ -1,6 +1,15 @@
-print("\"I'm\"")
-print("\"\"learning\"\"")
-print("\"\"\"Python\"\"\"\"")
+john = 3
+mary = 5
+adam = 6
+print (john, mary, adam, sep = ",")
 
-print()
-print("\"I'm\"\n","\"\"learning\"\"\n","\"\"\"Python\"\"\"\"\n")
+total_apples = john + mary + adam
+print (total_apples)
+
+print ("Mary has:", mary - john , "more apples than John.")
+print ("John and Mary has:", mary+john, "apples.")
+print ("John has half the number of apples Adam has:", adam/2, "apples.")
+print ("If we multiply the number of apples of John and Mary, the total will be:", john*mary, "apples.")
+print ("If we divide the number of apples of Adam and Mary, the number will be:", adam/mary, "apples. A float number!")
+print ("The integer divisional of Adam and John is: ", adam//john)
+print ("The number of apples of Adam and John, minus the number of apples of Mary is:", ((adam+john) - mary), "apples.")
