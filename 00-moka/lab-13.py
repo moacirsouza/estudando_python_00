fbf5a1af84ab9d8e48fe9aed866b23827109f8f2
Moka <25541880+moacirsouza@users.noreply.github.com>
2026-01-12 08:25:31 -0300

    Cria o arquivo lab-13.py

---
+++ 00-moka/lab-13.py
@@ -0,0 +1,19 @@
+### Valores de referência
+# +---------------+---------------------------------------+
+# |     Input     |            Expected Output            |
+# +---------------+---------------------------------------+
+# | spathiphyllum |       No, I want a big Spathiphyllum! |
+# +---------------+---------------------------------------+
+# | pelargonium   |       Spathiphyllum! Not pelargonium! |
+# +---------------+---------------------------------------+
+# | Spathiphyllum | Spathiphyllum is the best plant ever! |
+# +---------------+---------------------------------------+
+
+plant_name = input("What's your favourite plant? ")
+
+if plant_name == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif plant_name == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not " + plant_name + "!")
