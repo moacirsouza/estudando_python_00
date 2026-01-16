d85c7228dc5774140fd16b5e22252d3905eeb80c
Moka <25541880+moacirsouza@users.noreply.github.com>
2026-01-16 08:16:03 -0300

    Cria um código mais robusco para o lab-14.py

--- 00-moka/lab-14.py
+++ 00-moka/lab-14.py
@@ -1,12 +1,16 @@
 income = float(input("Enter the annual income: "))
 
+# All values in accordance with bill 123.456/1865
 tax_cap = 85_528
-tax_for_higher_incomes = 14_839.02
+tax_relief = 556.02
+tax_percentage_below_cap = 0.18
+tax_for_higher_incomes = 12_839.02
+tax_percentage_above_cap = 0.32
 
 if income <= tax_cap:
-    tax = ( 0.18 * income ) - 556.02
+    tax = ( tax_percentage_below_cap * income ) - tax_relief
 else:
-    tax = tax_for_higher_incomes + 0.32 * ( income - tax_cap )
+    tax = tax_for_higher_incomes + tax_percentage_above_cap * ( income - tax_cap )
 
 if tax < 0:
     tax = 0
