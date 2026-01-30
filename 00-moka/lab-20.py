45743d770245183817069c6942cc1aa22542c1ad
Moka <25541880+moacirsouza@users.noreply.github.com>
2026-01-30 08:18:23 -0300

    Cria o arquivo lab-20.py

---
+++ 00-moka/lab-20.py
@@ -0,0 +1,16 @@
+word_without_vowels = ""
+
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+user_word = input("Enter a word: ").upper()
+
+for letter in user_word:
+    # Complete the body of the loop.
+    if letter in "AEIOU":
+        continue
+
+    word_without_vowels += letter
+
+# Print the word assigned to word_without_vowels.
+print(word_without_vowels)
+
