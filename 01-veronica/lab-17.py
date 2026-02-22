7d22cbe4808f7a0172f54b3b8c39da75f53bcb28
Veronica <veronicapaviani@gmail.com>
2026-02-22 20:15:50 -0300

    Novo exercicio do lab-17

---
+++ 01-veronica/lab-17.py
@@ -0,0 +1,14 @@
+import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
+
+for count_mississippi in range(5):
+    print(count_mississippi + 1, "Mississippi")
+    time.sleep(1)
+
+ # The final message is:
+print("Ready or not, here I come!")
